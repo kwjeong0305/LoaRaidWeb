c78d1610a6e967b7d2e1171642020e04c8671917
kwjeong <kwanwoo2450@gmail.com>
2022-10-02 23:50:24 +0900

    Spring CORS 설정

--- src/main/java/com/asacoa/loaraidweb/config/WebConfig.java
+++ src/main/java/com/asacoa/loaraidweb/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.asacoa.loaraidweb.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,7 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/static/vue/static/", "classpath:/templates/vue/")
                 .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
     }
 }
--- src/main/java/com/asacoa/loaraidweb/controller/WebController.java
+++ src/main/java/com/asacoa/loaraidweb/controller/WebController.java
@@ -1,11 +1,9 @@
 package com.asacoa.loaraidweb.controller;
 
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@AllArgsConstructor
 public class WebController {
 
     @GetMapping("/vue")
