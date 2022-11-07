7b2e41d6a92f342d13febde40f3b547e1aa4c316
kwjeong <kwjeong@steg.co.kr>
2022-11-07 17:08:28 +0900

    레이드 수정 Modal창에서 값 변경시 원본 데이터도 변경되는 현상 수정

--- vue/src/components/UpdateRaid.vue
+++ vue/src/components/UpdateRaid.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="div-containerCard raid-add">
     <div class="div-containerItem raid-add-type raid-add-btn">
-      <select v-bind:value="raidData.raidType" disabled>
+      <select v-model="raidUpdateData.raidType">
         <option v-for="id in Object.keys(raidList)" :value="id">{{ id }}</option>
       </select>
       <button class="btn-positive" @click="raidUpdateApi">수정</button>
     </div>
-    <div class="div-containerItem raid-add-difficulty" v-if="raidData.raidType !== ''">
-      <label class="label-raid-add" v-for="item in raidList[raidData.raidType].difficulty">
+    <div class="div-containerItem raid-add-difficulty" v-if="raidUpdateData.raidType !== ''">
+      <label class="label-raid-add" v-for="item in raidList[raidUpdateData.raidType].difficulty">
         <input id="" class="input-radio input-radio-difficulty" type="radio" name="difficulty"
                :value="item.id"
-               v-bind:checked="raidData.raidDifficulty==item.id">
+                :checked="raidData.raidDifficulty==item.id"
+               v-model="raidUpdateData.raidDifficulty">
         <span>{{ item.name }}</span>
       </label>
     </div>
     <div class="div-containerItem raid-add-members">
       <input class="input-text input-text-member" type="text" placeholder="공격대원을 입력해 주세요."
              v-on:change="charterApi"
-             v-for="i in raidList[raidData.raidType].maxMember"
+             v-for="i in raidList[raidUpdateData.raidType].maxMember"
              :id="i-1"
-             v-bind:value="raidData.raidMembers[i-1]">
+             v-model="raidUpdateData.raidMembers[i-1]">
     </div>
   </div>
 </template>
@@ -28,21 +29,19 @@
 export default {
   name: "UpdateRaid",
   props: {
-    raidData: Object,
-    raidList: Object
+    raidList: Object,
+    raidData: Object
   },
   data() {
     return {
       // 레이드 ID, 레이드 타입, 난이도, 공격대원
-      raidUpdateData: {
-        "raidId": '',
-        "raidType": '',
-        "raidDifficulty": '',
-        "raidMembers": []
-      },
+      raidUpdateData: this.copyObject(this.raidData)
     }
   },
   methods: {
+    copyObject(json) {
+      return JSON.parse(JSON.stringify(json));
+    },
     raidUpdateApi() {
       $.ajax({
         url: "http://localhost:8080/raid/update",
@@ -56,14 +55,11 @@ export default {
           console.log(data);
         }
       });
-    }
-    ,
+    },
     charterApi() {
       console.log('Call API charterApi');
-    }
-    ,
-  }
-  ,
+    },
+  },
 }
 </script>
 
